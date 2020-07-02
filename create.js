5d82c8d09c67d6dfbefebeb0dbfda85247c76247
lucaspere <lucasrupestre@hotmail.com>
2020-07-02 02:49:06 -0300

    creating the lambda function to dynamoDB

---
+++ create.js
@@ -0,0 +1,43 @@
+import * as uuid from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+   const data = JSON.parse(event.body);
+
+   const params = {
+      TableName: process.env.TableName,
+      Item: {
+         userId: event.requestContext.identity.cognitoIdentityId,
+         noteId: uuid.v1(),
+         content: data.content,
+         attachment: data.attachment,
+         createAt: Date.now()
+      }
+   };
+
+   dynamoDB.put(params, (error, data) => {
+      const headers = {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Credentials": true
+      };
+
+      if (error) {
+         const response = {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ status: false })
+         };
+         callback(null, response);
+         return;
+      }
+
+      const response = {
+         statusCode: 200,
+         headers,
+         body: JSON.stringify(params.Item)
+      };
+      callback(null, response);
+   });
+}
