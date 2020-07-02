d7d4e75d90a8c0495d1d7bd34e692604722dba15
lucaspere <lucasrupestre@hotmail.com>
2020-07-02 12:49:18 -0300

    adding delete file

---
+++ delete.js
@@ -0,0 +1,17 @@
+import handler from "./libs/handler-lib";
+import dynamoDB from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+
+   const params = {
+      TableName: process.env.tableName,
+      Key: {
+         userId: event.requestContext.identity.cognitoIdentityId,
+         noteId: event.pathParameters.id
+      }
+   };
+
+   await dynamoDB.delete(params);
+
+   return { status: true };
+});
